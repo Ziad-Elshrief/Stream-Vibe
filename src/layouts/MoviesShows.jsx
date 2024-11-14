4c7ae3083ec43040206bdd08a100cf308c22898b
Ziad Elshrief <zyad2100@gmail.com>
2024-11-14 17:22:05 +0200

    Finished MoviesShows , SiteInfo , Trial components

--- src/layouts/MoviesShows.jsx
+++ src/layouts/MoviesShows.jsx
@@ -3,11 +3,11 @@ import { popularMovies, popularShows } from "../data/popular";
 
 export default function MoviesShows() {
   return (
-    <main>
+    <main className="mb-20">
       <div className="container">
-        <h1 className="red-btn mb-3">Shows </h1>
+        <h1 className="w-fit text-sm  bg-red45 text-white px-4 py-3.5 rounded-lg outline-none border-0 focus-within:border focus-within:border-black15 mb-4">Shows </h1>
         <Popular type="show" title="Popular Shows" list={popularShows} />
-        <h1 className="red-btn mb-3">Movies </h1>
+        <h1 className="w-fit text-sm  bg-red45 text-white px-4 py-3.5 rounded-lg outline-none border-0 focus-within:border focus-within:border-black15 mb-4">Movies </h1>
         <Popular type="movie" title="Popular Movies" list={popularMovies} />
       </div>
     </main>
