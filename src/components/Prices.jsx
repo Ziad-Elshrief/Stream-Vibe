4ceed2888841f9251204724e9abbbb6e3c380422
Ziad Elshrief <zyad2100@gmail.com>
2024-11-14 17:36:31 +0200

    Finished prices and pricing plan components

--- src/components/Prices.jsx
+++ src/components/Prices.jsx
@@ -1,21 +1,41 @@
 import PricingPlan from "./PricingPlan";
 
 export default function Prices() {
-    return (
-      <main className="prices">
+  return (
+    <main className="mb-20">
       <div className="container">
-        <h2>Choose the plan that&apos;s right for you</h2>
-        <p>
+        <h2 className="font-bold text-[1.75rem] mb-4">
+          Choose the plan that&apos;s right for you
+        </h2>
+        <p className="text-gray60 text-balance">
           Join StreamVibe and select from our flexible subscription options
           tailored to suit your viewing preferences. Get ready for non-stop
           entertainment!
         </p>
-        <div className="plans three-grid">
-          < PricingPlan title="Basic Plan" description={"Enjoy an extensive library of movies and shows, featuring a range of content."} price={"9.99"} />
-          < PricingPlan title="Standard Plan" description={" Access to a wider selection of movies and shows, including most new releases and exclusive content."} price={"12.99"} />
-          < PricingPlan title="Premium Plan" description={"Access to a widest selection of movies and shows, including all new releases and Offline Viewing."} price={"14.99"} />
+        <div className="mt-[60px] grid gap-5 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+          <PricingPlan
+            title="Basic Plan"
+            description={
+              "Enjoy an extensive library of movies and shows, featuring a range of content."
+            }
+            price={"9.99"}
+          />
+          <PricingPlan
+            title="Standard Plan"
+            description={
+              " Access to a wider selection of movies and shows, including most new releases and exclusive content."
+            }
+            price={"12.99"}
+          />
+          <PricingPlan
+            title="Premium Plan"
+            description={
+              "Access to a widest selection of movies and shows, including all new releases and Offline Viewing."
+            }
+            price={"14.99"}
+          />
         </div>
       </div>
     </main>
-    );
-  }
+  );
+}
--- src/components/PricingPlan.jsx
+++ src/components/PricingPlan.jsx
@@ -1,16 +1,18 @@
-export default function PricingPlan({title , description , price}) {
+export default function PricingPlan({ title, description, price }) {
   return (
-    <div className="plan-card">
-      <h3>{title}</h3>
-      <p>
-        {description}
-      </p>
-      <h4>
-        ${price} <span>/Month</span>
+    <div className="bg-black10 border border-black15 rounded-[10px] p-10 shadow-md">
+      <h3 className="text-[1.75rem] mb-3">{title}</h3>
+      <p className="text-gray60 text-balance">{description}</p>
+      <h4 className="text-3xl my-10 ">
+        ${price} <span className="text-base text-gray60 ">/Month</span>
       </h4>
-      <div className="buttons d-flex gap-2">
-        <button className="black-btn">Start Free Trial</button>
-        <button className="red-btn">Choose Plan</button>
+      <div className="flex gap-2">
+        <button className=" w-fit text-sm  bg-black08 text-white px-4 py-3.5 rounded-lg outline-none border border-black15 focus-within:border-red45">
+          Start Free Trial
+        </button>
+        <button className="w-fit text-sm  bg-red45 text-white px-4 py-3.5 rounded-lg outline-none border-0 focus-within:border focus-within:border-black15">
+          Choose Plan
+        </button>
       </div>
     </div>
   );
