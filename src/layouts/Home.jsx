aedad58dae0ac1826d4ae45f805b91aadb2d3f15
Ziad Elshrief <zyad2100@gmail.com>
2024-11-14 15:21:03 +0200

    Finished Not found and landing components

--- src/layouts/Home.jsx
+++ src/layouts/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <Landing>
-        <img src={Logo} alt="Logo" id="logo" />
+        <img src={Logo} alt="Logo" className="absolute top-1/2 left-1/2 z-3 w-1/4 opacity-85 -translate-x-2/4 -translate-y-2/4 "/>
       </Landing>
       <SiteInfo />
       <Gernes />
