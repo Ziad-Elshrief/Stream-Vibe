466b5e7f5849072f747fd816b7f05d6a85808712
Ziad Elshrief <zyad2100@gmail.com>
2024-11-16 13:14:35 +0200

    Fixed  not found svg size for mobile

--- src/layouts/NotFound.jsx
+++ src/layouts/NotFound.jsx
@@ -5,11 +5,11 @@ export default function NotFound() {
     <>
       <Landing>
         <div className="absolute top-1/2 left-1/2 z-20 -translate-x-2/4 -translate-y-2/4 text-center">
-          <TbError404 className="block size-48 text-red45 fill-black06" />
-          <h2 className="font-bold text-2xl w-fit">Page Not Found</h2>
+          <TbError404 className="block size-32 sm:size-48 text-red45 fill-black06" />
+          <h2 className="font-bold text-lg sm:text-2xl w-fit">Page Not Found</h2>
         </div>
       </Landing>
-      
+
     </>
   );
 }
